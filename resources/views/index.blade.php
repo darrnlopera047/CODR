100bd73bb39f2a852392a286a77aae08416f7660
darrnlopera047 <darrn.lopera047@gmail.com>
2023-03-30 15:52:48 +0800

    commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,417 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <meta charset="UTF-8" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Codr </title>
+  
+    <link href="https://cdn.lineicons.com/3.0/lineicons.css" rel="stylesheet">
+      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <link rel="stylesheet" href="experinces.css">
+     <link rel="stylesheet" href="footer.css">
+    <link rel="stylesheet" href="ourservices.css" />
+    <link rel="stylesheet" href="css/styles.css" />
+      <link rel="stylesheet" href="slider/ourworkslider.css" />
+    <link rel="stylesheet" href="aboutus.css" />
+    <link rel="stylesheet" href="contact.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.2/TweenMax.min.js"></script>
+
+     <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+
+
+
+
+  </head>
+  <body>
+
+    <div class="container">
+      <div class="menu-container">
+        <div class="menu">
+          <div class="menu-shrink">
+            <div class="menu-left">
+              
+              <div class="menu-brand"><img src="logo/Logo.svg" width="60px" height="30px"></div>
+            </div>
+            <div class="menu-center">
+             
+           
+              <a id="Home" href="#">Home</a>
+             <a id="nav1" href="#">Services</a>
+              <a id="nav1" href="#">About</a>
+              <a id="nav1" href="#">Our Works</a>
+               <a id="nav1" href="#">Contact Us</a>
+            
+
+            </div>
+            <div class="menu-right">
+            <div class="specialpromo">Special Promo</div>
+            </div>
+          </div>
+
+       
+        </div>
+      </div>
+           
+      <div class="text-container">
+       
+          <div class="container-front">
+            <div class="box1">
+              <div class="titlefront"><span class="code">Code</span> that </br>drives results</div>
+<div class="paragraphfront">At Codr, we bring your vision to life with cutting-edge technology solutions. </br>Our team of experts in custom systems, mobile apps, and graphic design will </br>help elevate your business to new heights. With a commitment to quality and </br>uniqueness, your project will be a standout success. Partner with Codr for</br> results-driven code and experience the power of technology today.</div>
+<div class="buttonfront">Book A Call - Free Consultation</div>
+            </div>
+            <div class="box2">
+              <img src="img/call-us2.png" height="auto" width="100%">
+            </div>
+
+
+<!---experince------>
+
+
+<div class="space"></div>
+          </div>
+      <div class="Section_1">
+
+  <div class="FEATURES">
+    <div class="btn-features">FEATURES</div>
+
+  </div>
+  <div class="Trust">
+    <div>A Partner You Can <span class="a">Trust</span></div>
+    
+  </div>
+  <div>
+    <p class="paragraph">To help drive your business forward. With a commitment to delivering top-quality solutions,<br>
+    we are dedicated to making your vision a reality</p>
+    
+  </div>
+ 
+    <section class="Section1">
+  
+    <div class="wrapper">
+
+      <div class="box-area">
+
+        <div class="icon-area">
+            <div class="img-feature1"></div>
+        </div>
+        <div class="F1">Reliable</div>
+         <p class="paragraph-feature">We understand the importance of having a dependable partner to help you achieve your business goals. Our commitment to providing quality services ensures that you can trust us to deliver on our promises.</p>
+      </div>
+      <div class="box-area custom">
+        <div class="icon-area">
+         <div class="img-feature2"></div>
+        </div>
+         <div class="F1">Experienced</div>
+       
+         <p class="paragraph-feature">With years of experience in the industry, our team has the expertise needed to tackle any project, big or small. Whether you’re looking for a custom system, mobile app, or brand redesign, we’ve got you covered.</p>
+      </div>
+      <div class="box-area">
+        <div class="icon-area">
+           <div class="img-feature3"></div>
+        </div>
+        <div class="F1">Innovative</div>
+        
+        <p class="paragraph-feature">We believe in staying ahead of the curve, which is why we continuously explore new technologies and incorporate them into our solutions. With us, you can be confident that you’re getting the latest and greatest.</p>
+      </div>
+    </div> 
+
+    </section>   
+
+</div>
+      
+        
+        
+      </div>
+    </div>
+
+
+
+<!---our service------>
+  <div class="ourservices-a">
+
+    <div class="btn-ourservices">OUR SERVICES</div>
+
+  </div>
+  <div class="Give">
+    <div>We Give <span class="ab">Solutions</span></div>
+    
+  </div>
+  
+    
+  </div>
+</div>
+
+
+
+ <div class="container-ourservices">
+   
+
+
+
+        <div class="box-ourservices">
+          <div class="circle">
+            <img src="img/1.png" width="170px" height="170px">
+
+          </div>
+          <div class="title-ourservices">Brand Identity</div>
+          <p class="paragraph-ourservices">Elevate your brand with our expert brand identity services. Create a powerful emotional connection...</p>
+          <div class="readme"><a class="readme" href="#">Read me</a></div>
+        </div>
+        <div class="box-ourservices">
+          <div class="circle">
+            <img src="img/2.png" width="140px" height="140px">
+            
+          </div>
+          <div class="title-ourservices">UX/UI Design</div>
+          <p class="paragraph-ourservices">brand’s visual appeal with Codr’s UX/UI design..</p>
+          <div class="readme"><a class="readme" href="#">Read me</a></div>
+        </div>
+
+
+         <div class="box-ourservices">
+          <div class="circle">
+            <img src="img/3.png" width="180px" height="180px">
+          </div>
+          <div class="title-ourservices">Web & Mobile Development</div>
+          <p class="paragraph-ourservices">Our team of experts delivers custom solutions for your business, with a focus on elevating your online..</p>
+          <div class="readme"><a class="readme" href="#">Read me</a></div>
+        </div>
+        <div class="box-ourservices">
+          <div class="circle">
+            <img src="img/4.png" width="180px" height="180px">
+          </div>
+          <div class="title-ourservices">Custom System</div>
+          <p class="paragraph-ourservices">Transform the way you do business with our custom system solutions. Our team of experts will work..</p>
+          <div class="readme"><a class="readme" href="#">Read me</a></div>
+        </div>
+ </div>
+
+
+
+
+</div>
+</div>
+
+</div>
+</div>
+
+</div>
+
+
+<!---About Us------>
+
+
+<div class="aboutus">
+  <div class="box-aboutus1">
+   
+
+    <div class="btn-aboutus">About Us</div>
+
+ 
+    <div class="title-aboutus">Driving <span class="success">success</span> through technology</div>
+    <p>At Codr, our mission is to simplify the software development process for businesses and help turn their vision into a reality. With a team of experienced developers, designers, and project managers, we provide a full range of services including custom systems, mobile apps, graphic design, and more. Let us drive results for you.</p>
+   <button>
+    Learn More
+    <div class="arrow-wrapper">
+        <div class="arrow"></div>
+
+    </div>
+</button>
+  </div>
+
+
+
+</button>
+<div class="box-aboutus2"><img src="img/call-us3.png" height="auto" width="100%"></div>
+
+
+</div>
+
+
+
+<div>
+
+<div class="bg-ourwork">
+
+
+  <div class="ourworks1">
+    <div class="btn-ourworks1">OUR WORKS</div>
+
+  </div>
+  <div class="Accomplished1">
+    <div>Accomplished  <span class="xy">  Ventures</span></div>
+    
+  </div>
+ 
+
+
+
+<div class="container">
+    <div class="swiper-custom">
+        <!-- Slider main container -->
+        <div class="swiper mySwiper">
+            <!-- Additional required wrapper -->
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+               <div></div>
+                </div>
+                <div class="swiper-slide">
+         <div></div>                </div>
+                <div class="swiper-slide">
+               <div></div>
+                </div>
+                <div class="swiper-slide">
+                        <div></div>
+                </div>
+                <div class="swiper-slide">
+                 <div></div>
+                </div>
+                <div class="swiper-slide">
+                        <div></div>
+                </div>
+
+
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            </div>
+
+
+
+        </div>
+
+        
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<div class="za-container">
+          <div class="boxslider1">
+            <img id="swiper-button-prev-1" src="img/left.svg" height="100%" width="100%">
+          </div>
+           <div class="boxslider2">
+             <img id="swiper-button-next-1" src="img/right.svg" height="100%" width="100% ">
+           </div>
+
+
+        </div>
+
+
+
+</div>
+
+  </div>  
+  </div>
+
+</div>
+
+
+
+</div>
+
+
+<div class="space"></div>
+
+<div class="boxxcontainer">
+<div class="boxslider1"></div>
+<div class="boxslider2"></div>
+<div class="boxslider3"></div>
+<div class="boxslider4"></div>
+
+   </div>
+  
+  
+
+</div>
+
+   
+
+
+
+
+
+
+  <footer class="footer">
+     <div class="containerfooter">
+      <div class="row">
+        <div class="footer-col">
+          <h4>QUICK LINKS</h4>
+          <ul class="ulfooter">
+            <li class="lifooter"> <a class="afooter" href="#">Home</a></li>
+            <li class="lifooter"> <a class="afooter" href="#">Services</a></li>
+         <li class="lifooter">  <a class="afooter" href="#">About</a></li>
+            
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>&nbsp&nbsp&nbsp&nbsp&nbsp</h4>
+          <ul class="ulfooter">
+            <li class="lifooter">  <a class="afooter" href="#">Our Works</a></li>
+          <li class="lifooter"> <a class="afooter" href="#">Pricing</a></li>
+          <li class="lifooter">  <a class="afooter" href="#">Book a Call</a></li>
+         
+          </ul>
+        </div>
+        <div class="footer-col">
+           <h4>&nbsp&nbsp&nbsp&nbsp&nbsp</h4>
+          <ul class="ulfooter">
+            <li class="lifooter"> <a class="afooter" href="#">FAQs</a></li>
+          <li class="lifooter"><a  class="afooter" href="#">Privacy Policy</a></li>
+          <li class="lifooter"><a class="afooter" href="#">Terms & Conditions</a></li>
+           
+          </ul>
+        </div>
+        <div class="footer-col">
+          <div class="social">Social<span class="media">Media</span> </div>
+          <div class="social-links">
+            <a class="afooter"  href="#"><i  class="fab fa-instagram"></i></a>
+             <a class="afooter"  href="#"><i class="fab fa-twitter"></i></a>
+            <a class="afooter" href="#"><i class="fab fa-facebook-f"></i></a>
+           
+            
+          </div>
+        </div>
+      </div>
+     </div>
+  </footer>
+
+
+
+
+  </body>
+
+
+
+
+
+</html>
+
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<script src="slider/custom.js"></script>
